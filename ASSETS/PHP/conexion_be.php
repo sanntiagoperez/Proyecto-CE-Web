0f38a34c05259b6646f9883f5314606654883ff6
Lincoln Castro Esquivel <castroesquivellincolneduardo@gmail.com>
2026-08-02 19:55:44 -0500

    conexion a PostgreSQL configurada y probada exitosamente

--- ASSETS/PHP/conexion_be.php
+++ ASSETS/PHP/conexion_be.php
@@ -1,12 +1,18 @@
 <?php
-  
+$host = "localhost";
+$port = "5432";
+$dbname = "postgres"; 
+$user = "postgres";
+$password = "1508"; 
 
-   $conexion = mysqli_connect("localhost", "root", "", "ce_web");
 
-   if($conexion){
-        echo 'Conectado exitosamente a la Base de Datos';
-   }else{
-        echo 'No se ha podido conectar a la Base de Datos';
-   }
+$connection_string = "host=$host port=$port dbname=$dbname user=$user password=$password";
 
+$conexion = pg_connect($connection_string);
+
+if($conexion){
+     echo 'Conectado exitosamente a la Base de Datos de PostgreSQL';
+}else{
+     echo 'No se ha podido conectar a la Base de Datos de PostgreSQL';
+}
 ?>
